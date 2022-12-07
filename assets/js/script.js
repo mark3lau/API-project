2969daa3290929457826867a129629cb86e5dba6
mark3lau <mark3lau@gmail.com>
2022-12-07 16:06:41 +0000

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const API_KEY = "Dwgzgtl6J5D88bZdKCfXJBQvNrQ";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+
